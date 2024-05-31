a2198e731ed86f330807e6608785d8c452a6dcd2
Mikee131 <michaeleshete9@gmail.com>
2024-05-31 15:07:45 +0300

    chore: Saving on the dataStore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { DataQuery, useConfig } from '@dhis2/app-runtime'
-import i18n from '@dhis2/d2-i18n'
+import { DataQuery, useDataMutation, useAlert } from '@dhis2/app-runtime'
+import { Button, InputField, SingleSelectField, SingleSelectOption, Transfer } from '@dhis2/ui'
 import classes from './App.module.css'
-import { Button, Card, Checkbox, InputField, SingleSelect, SingleSelectField, SingleSelectOption, TextArea, Transfer } from '@dhis2/ui'
 
 const query = {
     me: {
@@ -22,7 +21,6 @@ const query = {
             paging: false
         }
     },
-
     dataSets: {
         resource: 'dataSets',
         params: {
@@ -30,17 +28,13 @@ const query = {
             paging: false
         }
     },
-
     userGroups: {
         resource: 'userGroups',
         params: {
-
-
             fields: 'id,displayName',
             paging: false
         }
     },
-
     userRoles: {
         resource: 'userRoles',
         params: {
@@ -48,17 +42,52 @@ const query = {
             paging: false
         }
     },
-
 }
 
 
-function MyApp() {
-    const [configurationName, setConfigurationName] = useState(null)
+const saveMutation = {
+    type: 'update', 
+    resource: 'dataStore/myNamespace', 
+    id: ({ key }) => key, 
+    data: ({ data }) => data 
+}
+
+const MyApp = () => {
+    const [configurationName, setConfigurationName] = useState('')
     const [selectedOptionSets, setSelectedOptionSets] = useState({})
     const [selectedOUGs, setSelectedOUGs] = useState([])
     const [selectedDataSets, setSelectedDataSets] = useState([])
     const [selectedUserGroups, setSelectedUserGroups] = useState([])
     const [selectedUserRoles, setSelectedUserRoles] = useState([])
+
+    const successAlert = useAlert('Configuration saved successfully!', { duration: 3000 })
+    const errorAlert = useAlert('Failed to save configuration', { critical: true })
+
+    const [mutate, { loading: saving, error: saveError }] = useDataMutation(saveMutation)
+
+    const handleSave = async () => {
+        const toSave = {
+            name: configurationName,
+            selectedOptionSets,
+            selectedOUGs,
+            selectedDataSets,
+            selectedUserGroups,
+            selectedUserRoles
+        }
+
+        const key = `key_${Date.now()}`
+
+        try {
+            await mutate({
+                key,
+                data: toSave
+            })
+            successAlert.show()
+        } catch (error) {
+            console.error('Error saving configuration:', error)
+            errorAlert.show()
+        }
+    }
 
     return (
         <div className={classes.container}>
@@ -66,140 +95,129 @@ function MyApp() {
                 {({ error, loading, data }) => {
                     if (error) return <span>ERROR</span>
                     if (loading) return <span>...</span>
-                    {
-                        const server_OUGs = data.organisationUnitGroups.organisationUnitGroups.map(
-                            item => remapToLabelAndValue(item)
-                        )
-                        const server_dataSets = data.dataSets.dataSets.map(
-                            item => remapToLabelAndValue(item)
-                        )
-                        const server_userRoles = data.userRoles.userRoles.map(
-                            item => remapToLabelAndValue(item)
-                        )
-                        const server_userGroups = data.userGroups.userGroups.map(
-                            item => remapToLabelAndValue(item)
-                        )
+                    if (!data) return <span>No data available</span>
 
-                        const server_optionSets = data.optionSets.optionSets
+                    const server_OUGs = data.organisationUnitGroups?.organisationUnitGroups?.map(
+                        item => remapToLabelAndValue(item)
+                    ) || []
+                    const server_dataSets = data.dataSets?.dataSets?.map(
+                        item => remapToLabelAndValue(item)
+                    ) || []
+                    const server_userRoles = data.userRoles?.userRoles?.map(
+                        item => remapToLabelAndValue(item)
+                    ) || []
+                    const server_userGroups = data.userGroups?.userGroups?.map(
+                        item => remapToLabelAndValue(item)
+                    ) || []
+                    const server_optionSets = data.optionSets?.optionSets || []
 
-                        return (
-                            <>
-                                <InputField
-                                    label='Configuration name'
-                                    name="configurationName"
-                                    value={configurationName}
-                                    onChange={e => setConfigurationName(e.value)}
-                                    placeholder='Please provide a name for this configuration' />
-                                <br />
-                                {
-                                    server_optionSets && server_optionSets.map(
-                                        item => {
-                                            return (
-                                                <SingleSelectField
-                                                    label={item.displayName}
-                                                    selected={selectedOptionSets[item.id]}
-                                                    onChange={e => {
-                                                        let tempSelectedOptionSets = { ...selectedOptionSets }
-                                                        tempSelectedOptionSets[item.id] = e.selected
-                                                        setSelectedOptionSets(tempSelectedOptionSets)
-                                                    }}>
-                                                    {
-                                                        item.options.map(option => <SingleSelectOption label={option.displayName} value={option.code} />)
-                                                    }
-                                                </SingleSelectField>
+                    return (
+                        <>
+                            <InputField
+                                label='Configuration name'
+                                name="configurationName"
+                                value={configurationName}
+                                onChange={e => setConfigurationName(e.value)}
+                                placeholder='Please provide a name for this configuration' />
+                            <br />
+                            {
+                                server_optionSets.map(item => (
+                                    item && item.options && item.displayName && 
+                                    <SingleSelectField
+                                        key={item.id}
+                                        label={item.displayName}
+                                        selected={selectedOptionSets[item.id]}
+                                        onChange={e => {
+                                            let tempSelectedOptionSets = { ...selectedOptionSets }
+                                            tempSelectedOptionSets[item.id] = e.selected
+                                            setSelectedOptionSets(tempSelectedOptionSets)
+                                        }}>
+                                        {
+                                            item.options.map(option => option && option.displayName && 
+                                                <SingleSelectOption 
+                                                    key={option.id} 
+                                                    label={option.displayName} 
+                                                    value={option.code} 
+                                                />
                                             )
                                         }
-                                    )
-                                }
-                                {
-                                    server_OUGs &&
-                                    <>
-                                        <br />
-                                        Organisation unit groups
-                                        <Transfer
-                                            options={server_OUGs}
-                                            selected={selectedOUGs}
-                                            onChange={(e) => {
-                                                setSelectedOUGs(e.selected)
-                                            }}
-
-                                        />
-                                    </>
-                                }
-                                {
-                                    server_dataSets &&
-                                    <>
-                                        <br />
-                                        Data sets
-                                        <Transfer
-                                            options={server_dataSets}
-                                            selected={selectedDataSets}
-                                            onChange={(e) => {
-                                                setSelectedDataSets(e.selected)
-                                            }}
-
-                                        />
-                                    </>
-                                }
-
-                                {
-                                    server_userGroups &&
-                                    <>
-                                        <br />
-                                        User Groups
-                                        <Transfer
-                                            options={server_userGroups}
-                                            selected={selectedUserGroups}
-                                            onChange={(e) => {
-                                                setSelectedUserGroups(e.selected)
-                                            }}
-
-                                        />
-                                        <br />
-                                        User Roles
-                                        <Transfer
-                                            options={server_userRoles}
-                                            selected={selectedUserRoles}
-                                            onChange={(e) => {
-                                                setSelectedUserRoles(e.selected)
-                                            }}
-
-                                        />
-                                    </>
-                                }
-                                <br />
-                                <Button primary onClick={e => {
-
-                                    /**
-                                     * TODO: Save the object 'toSave' here on data store.
-                                     */
-
-                                    const toSave = {
-                                        name: configurationName,
-                                        selectedOptionSets,
-                                        selectedOUGs,
-                                        selectedDataSets,
-                                        selectedUserGroups,
-                                        selectedUserRoles
-                                    }
-                                }
-                                }>Save</Button>
-                                <br />
-                            </>
-
-                        )
-                    }
+                                    </SingleSelectField>
+                                ))
+                            }
+                            {
+                                server_OUGs.length > 0 &&
+                                <>
+                                    <br />
+                                    Organisation unit groups
+                                    <Transfer
+                                        options={server_OUGs}
+                                        selected={selectedOUGs}
+                                        onChange={(e) => {
+                                            setSelectedOUGs(e.selected)
+                                        }}
+                                    />
+                                </>
+                            }
+                            {
+                                server_dataSets.length > 0 &&
+                                <>
+                                    <br />
+                                    Data sets
+                                    <Transfer
+                                        options={server_dataSets}
+                                        selected={selectedDataSets}
+                                        onChange={(e) => {
+                                            setSelectedDataSets(e.selected)
+                                        }}
+                                    />
+                                </>
+                            }
+                            {
+                                server_userGroups.length > 0 &&
+                                <>
+                                    <br />
+                                    User Groups
+                                    <Transfer
+                                        options={server_userGroups}
+                                        selected={selectedUserGroups}
+                                        onChange={(e) => {
+                                            setSelectedUserGroups(e.selected)
+                                        }}
+                                    />
+                                </>
+                            }
+                            {
+                                server_userRoles.length > 0 &&
+                                <>
+                                    <br />
+                                    User Roles
+                                    <Transfer
+                                        options={server_userRoles}
+                                        selected={selectedUserRoles}
+                                        onChange={(e) => {
+                                            setSelectedUserRoles(e.selected)
+                                        }}
+                                    />
+                                </>
+                            }
+                            <br />
+                            <Button primary onClick={handleSave} disabled={saving}>
+                                {saving ? 'Saving...' : 'Save'}
+                            </Button>
+                            {saveError && <span>Error saving configuration</span>}
+                        </>
+                    )
                 }}
             </DataQuery>
-        </div >
+        </div>
     )
 }
 
 function remapToLabelAndValue(obj) {
-    const { displayName, id, ...rest } = obj;
+    const { displayName, id, ...rest } = obj || {}
     return {
-        label: displayName,
-        value: id,
+        label: displayName || '',
+        value: id || '',
         ...rest
     }
 }
